7cd4de9113426082f4dc8446b3145ec3da7e2bb5
gpspelle <gpsunicamp016@gmail.com>
2019-03-20 20:26:53 -0300

    creating database with C API

--- P1/sql/db.c
+++ P1/sql/db.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <my_global.h>
+#include <string.h>
 #include <mysql.h>
 
-int main(int argc, char **argv) {  
+#define user "root"
+#define pswd "123"
+#define network "localhost"
+
+void create_database(MYSQL *con) {  
+
+    if (mysql_real_connect(con, network, user, pswd, NULL, 0, NULL, 0) == NULL) {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    }  
+
+    char a[] = "CREATE database ";
+    char b[] = "MC833;";
+
+    strcat(a, b);
+
+    if (mysql_query(con, a)) {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    } else {
+        printf("Succesfully created Database MC833\n");
+    }
+
+    mysql_close(con);
+}
+
+int main() {
+
     MYSQL *con = mysql_init(NULL);
 
     if (con == NULL) {
@@ -11,19 +40,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    if (mysql_real_connect(con, "localhost", "root", "123", 
-                                NULL, 0, NULL, 0) == NULL) {
-        fprintf(stderr, "%s\n", mysql_error(con));
-        mysql_close(con);
-        exit(1);
-    }  
-
-    if (mysql_query(con, "CREATE database teste;")) {
-        fprintf(stderr, "%s\n", mysql_error(con));
-        mysql_close(con);
-        exit(1);
-    }
-
-    mysql_close(con);
-    exit(0);
+    create_database(con);
+    return 0;
 }
+
