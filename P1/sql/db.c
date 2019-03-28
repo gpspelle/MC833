9ddd7a6d1e0e613931103874416370f211043c61
gpspelle <gpsunicamp016@gmail.com>
2019-03-28 10:58:57 -0300

    [working] db with headers

--- P1/sql/db.c
+++ P1/sql/db.c
@@ -1,26 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <mysql.h>
-
-#define rootDB "root"
-#define pswd_root "123"
-#define userDB "redes"
-#define pswd "123"
-#define network "localhost"
-#define DB "MC833"
-#define tabelaUSER "Usuarios"
-#define tabelaEXP "Experiencias"
-#define tabelaHAB "Habilidades"
-
-typedef struct user {
-    char email[100];
-    char nome[100];
-    char sobrenome[100];
-    char foto[100];
-    char residencia[100];
-    char formacao[100];
-}userT;
+#include "db.h"
 
 void return_error(MYSQL *con) {
     fprintf(stderr, "%s\n", mysql_error(con));
