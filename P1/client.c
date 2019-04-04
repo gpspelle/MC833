646238faf1fc57e8f99e31117ead1cf2590fc9a9
gpspelle <gpsunicamp016@gmail.com>
2019-04-04 10:49:33 -0300

    [working] removing comments

--- P1/client.c
+++ P1/client.c
@@ -17,228 +17,6 @@ void get_time(FILE *fp) {
     fprintf(fp, "%s.%09ld;", buff+9, ts.tv_nsec);
 }
 
-/*int create_connection() {
-
-    int sockfd;
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    char s[INET6_ADDRSTRLEN];
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-
-    if ((rv = getaddrinfo("127.0.0.1", PORT, &hints, &servinfo)) != 0) {
-            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-            return;
-    }
-
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                        p->ai_protocol)) == -1) {
-            perror("client: socket");
-            continue;
-        }
-
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            perror("client: connect");
-            close(sockfd);
-            continue;
-        }
-
-        break;
-    }
-
-    if (p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
-        return;
-    }
-
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-                    s, sizeof s);
-    printf("client: connecting to %s\n", s);
-
-    freeaddrinfo(servinfo); // all done with this structure
-
-    return sockfd;
-}*/
-
-void client_call(char *input, int num_input, FILE *fp_output, int sockfd) {
-
-    char buf[MAX_DATA_SIZE] = "";
-    char *image;
-    //int sockfd;
-    long int numbytes = -1;  
-    //struct addrinfo hints, *servinfo, *p;
-    //int rv;
-    //char s[INET6_ADDRSTRLEN];
-
-    //memset(&hints, 0, sizeof hints);
-    //hints.ai_family = AF_UNSPEC;
-    //hints.ai_socktype = SOCK_STREAM;
-
-    /*if ((rv = getaddrinfo("177.220.84.162", PORT, &hints, &servinfo)) != 0) {
-            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-            return;
-    }*/
-
-    //get_time(fp_output);
-    /*if ((rv = getaddrinfo("127.0.0.1", PORT, &hints, &servinfo)) != 0) {
-            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-            return;
-    }
-
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                        p->ai_protocol)) == -1) {
-            perror("client: socket");
-            continue;
-        }
-
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            perror("client: connect");
-            close(sockfd);
-            continue;
-        }
-
-        break;
-    }
-
-    if (p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
-        return;
-    }
-
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-                    s, sizeof s);
-    printf("client: connecting to %s\n", s);
-
-    freeaddrinfo(servinfo); // all done with this structure
-
-    if (send(sockfd, input, MAX_DATA_SIZE, 0) == -1) {
-        perror("send");
-        exit(1);
-    }  */ 
-
-    char a[20] = "";
-    char b[20] = "";
-
-    while(1) {
-        char kkey0[1] = "";
-        kkey0[0] = '\0';
-        numbytes = recv(sockfd, kkey0, 1, 0); 
-        kkey0[1] = '\0';
-
-        if(*kkey0 == '@') {
-            break;
-        }
-
-        strcat(a, kkey0);
-    }
-
-    numbytes = atoi(a);
-    //printf("(%d) - [%s]\n", numbytes, a);
-    size_t bytesRead = 0;
-    size_t bytesToRead = numbytes;
-
-    while (bytesToRead != bytesRead) {
-        size_t readThisTime;
-        do {
-            readThisTime = recv(sockfd, buf + bytesRead, (bytesToRead - bytesRead), 0);
-        } while((readThisTime == -1) && (errno == EINTR));
-
-        if(readThisTime == -1) {
-            break;
-        }  
-
-        bytesRead += readThisTime;
-    }
-
-    numbytes = -1;
-
-    if(num_input == 6) {
-        char *email = strtok(input, ";");
-        email = strtok (NULL, ";");
-        char path[1000] = "dados/";
-        strcat(path, email); 
-        strcat(path, ".jpg");
-        struct stat st = {0};
-
-        if (stat("dados/", &st) == -1) {
-            mkdir("dados/", 0700);
-        }
-
-        FILE *fp = fopen(path, "wb");
-
-        while(1) {
-            char kkey1[1] = "";
-            kkey1[0] = '\0';
-            numbytes = recv(sockfd, kkey1, 1, 0); 
-            kkey1[1] = '\0';
-
-            if(*kkey1 == '@') {
-                break;
-            }
-        
-            strcat(b, kkey1);
-        }
-
-        numbytes = atoi(b);
-        image = malloc(sizeof(char)*(numbytes+1));
-
-        bytesRead = 0;
-        bytesToRead = numbytes;
-
-        while (bytesToRead != bytesRead) {
-            size_t readThisTime;
-            do {
-                readThisTime = recv(sockfd, image + bytesRead, (bytesToRead - bytesRead), 0);
-            } while((readThisTime == -1) && (errno == EINTR));
-
-            if(readThisTime == -1) {
-                break;
-            }
-
-            bytesRead += readThisTime;
-        }
-
-        get_time(fp_output);
-        fprintf(fp_output, "\n");
-
-        size_t bytesToWrite = numbytes;
-        size_t bytesWritten = 0;
-
-        while (bytesWritten != bytesToWrite) {
-            size_t writtenThisTime;
-
-            do {
-               writtenThisTime = fwrite(image, 1, (bytesToWrite - bytesWritten), fp);
-            } while((writtenThisTime == -1) && (errno == EINTR));
-
-            if(writtenThisTime == -1) {
-                break;
-            }
-
-            bytesWritten += writtenThisTime;
-        }
-
-        fclose(fp);
-        printf("[%s]\n", buf);
-        printf("(%d, %d, %d)\n", bytesRead, bytesToRead, strlen(buf));
-        close(sockfd);
-        return;
-    }
-
-    get_time(fp_output);
-    fprintf(fp_output, "\n");
-    printf("[%s]\n", buf);
-    printf("(%d, %d, %d)\n", bytesRead, bytesToRead, strlen(buf));
-    close(sockfd);
-
-}
-
 int main(int argc, char *argv[]) {
 
     setenv("TZ", "PST8PDT", 1);
@@ -321,8 +99,6 @@ int main(int argc, char *argv[]) {
         char email[100];
         char message[300] = "";
 
-        //printf("Input: [%d] ", input);
-
         switch(input) {
             case 1: printf("Qual curso: ");
                     fscanf(fi, " %[^\n]s", curso); 
@@ -386,11 +162,6 @@ int main(int argc, char *argv[]) {
             bytes_written += written;
         }
 
-        /*if (send(sockfd, message, MAX_DATA_SIZE, 0) == -1) {
-            perror("send");
-            exit(1);
-        }*/
-
         char a[20] = "";
         char b[20] = "";
 
@@ -408,7 +179,6 @@ int main(int argc, char *argv[]) {
         }
 
         numbytes = atoi(a);
-        //printf("(%d) - [%s]\n", numbytes, a);
         size_t bytesRead = 0;
         size_t bytesToRead = numbytes;
 
