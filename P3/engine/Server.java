82d1f6266995a5b2750f9736694c0b249f2a6b5d
Giovanna Vendramini <gigivendra@hotmail.com>
2019-06-25 16:48:54 -0300

    Add database

--- P3/engine/Server.java
+++ P3/engine/Server.java
@@ -10,6 +10,12 @@ import java.util.ArrayList;
 public class Server implements Interface {
 
     public Server() {
+
+        String url = "jdbc:mysql://localhost:3306/MC833";
+        String login = "root";
+        String senha = "123";
+        this.database banco = new database(url, login, senha);
+        this.banco.connect();
         super();
     }
 
@@ -17,46 +23,46 @@ public class Server implements Interface {
         //realizar consulta no banco de dados
         String resultado = null;
         if (req==1){
-            //List<String> lista = banco.op1(par1);
-            //for (String i : lista) {
-            //    resultado = resultado + i;
-            //}
+            List<String> lista = banco.op1(par1);
+            for (String i : lista) {
+                resultado = resultado + i;
+            }
             resultado = "recebi req 1";
             resultado = resultado + par1;
         }
         else if (req==2){
-            //List<String> lista = banco.op2(par1);
-            //for (String i : lista) {
-            //    resultado = resultado + i;
-            //}
+            List<String> lista = banco.op2(par1);
+            for (String i : lista) {
+                resultado = resultado + i;
+            }
             resultado = "recebi req 2";
         }
         else if (req==3){
-            //boolean success = banco.op3(par1, par2);
-            //if (success = true) {
-            //    resultado = "Sucesso";
-            //}
-            //else {
-            //    resultado = "Falha";
-            //}
+            boolean success = banco.op3(par1, par2);
+            if (success = true) {
+                resultado = "Sucesso";
+            }
+            else {
+                resultado = "Falha";
+            }
             resultado = "recebi req 3";
         }
 
         else if (req==4){
-            //resultado = banco.op4(par1);
+            resultado = banco.op4(par1);
             resultado = "recebi req 4";
         }
 
         else if (req==5) {
-            //List<String> lista = banco.op5();
-            //for (String i : lista) {
-            //    resultado = resultado + i;
-            //}
+            List<String> lista = banco.op5();
+            for (String i : lista) {
+                resultado = resultado + i;
+            }
             resultado = "recebi req 5";
         }
 
         else if (req==6){
-            //resultado = banco.op6(par1);
+            resultado = banco.op6(par1);
             resultado = "recebi req 6";
         }
         return resultado;
@@ -76,6 +82,5 @@ public class Server implements Interface {
             System.err.println("Server exception:");
             e.printStackTrace();
         }
-        //database banco = new database();
     }
 }
